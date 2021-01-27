30a6a7dc24f5aec89aeac141a7d077481c668a22
Mikel <gusidam@gmail.com>
2021-01-27 14:57:09 +0000

    now rendering from arrays

--- js/App.js
+++ js/App.js
@@ -1,14 +1,18 @@
 import Score from "./Score.js";
 import Composer from "./Composer.js";
 
+const tableBodyNode = document.getElementById("score-table-body");
+const composerSelect = document.getElementById("new-score-composer");
+const styleSelect = document.getElementById("new-score-style");
+const instrumentsSelect = document.getElementById("new-score-instrumentation");
 class App {
   scores = [];
   composers = [];
-  instruments = ["Oboe", "Piano"];
+  instruments = ["Oboe", "Piano", "drum"];
   styles = ["Classic", "Romantic", "Jazz"];
   constructor({ name, password }) {
     // Defensive Checks
-    console.log(document);
+
     if (name == null) {
       throw new Error(`accounts require a name, received ${name}`);
     }
@@ -20,26 +24,36 @@ class App {
   }
 
   //Create
-  createScore(
-    newScore = { title, composer, style, stock, instrumentation, owner },
-  ) {
-    newScore = new Score(newScore);
-    this.scores.push(newScore);
-    return newScore;
+  addScore(score) {
+    if (score instanceof Score) {
+      this.scores.push(score);
+      this.renderScoreTable();
+      return score;
+    } else {
+      throw new Error(`score needed, received  ${score}`);
+    }
   }
-
-  createComposer(
-    newComposer = new Composer({
-      name,
-      isFemale,
-      period,
-      nationality,
-      funFact,
-    }),
-  ) {
-    this.composers.push(newComposer);
-    return newComposer;
+  addComposer(composer) {
+    if (composer instanceof Composer) {
+      this.composers.push(composer);
+      this.renderComposerSelect();
+      return composer;
+    } else {
+      throw new Error(`composer needed, received  ${composer}`);
+    }
   }
+  // addComposer(
+  //   newComposer = new Composer({
+  //     name,
+  //     isFemale,
+  //     period,
+  //     nationality,
+  //     funFact,
+  //   }),
+  // ) {
+  //   this.composers.push(newComposer);
+  //   return newComposer;
+  // }
   //Read
   getAllScores() {
     return this.scores;
@@ -72,19 +86,73 @@ class App {
   //   return this.scores.splice(index, 1);
   // }
   /* -------------------Render bits-----------------------------*/
+  renderComposerSelect() {
+    let injectedString = "";
+    for (const composer of this.composers) {
+      injectedString += `<option>${composer.name}</option> `;
+    }
+    composerSelect.innerHTML = injectedString;
+  }
+  renderStyleSelect() {
+    let injectedString = "";
+    for (const style of this.styles) {
+      injectedString += `<option>${style}</option> `;
+    }
+    styleSelect.innerHTML = injectedString;
+  }
+  renderInstrumentsSelect() {
+    let injectedString = "";
+    for (const instrument of this.instruments) {
+      injectedString += `<option>${instrument}</option>`;
+    }
+    instrumentsSelect.innerHTML = injectedString;
+  }
+  renderScoreTable() {
+    let injectedString = "";
+    for (const score of this.scores) {
+      let concatenatedInstruments = "";
+      for (const instrument of score.instrumentation) {
+        concatenatedInstruments += ` ${instrument},`;
+      }
+
+      injectedString += `<tr>
+          <th>${score.title}</th>
+          <td>${score.composer.name}</td>
+          <td>${score.style}</td>
+          <td>${concatenatedInstruments.slice(0, -1)}</td>
+          <td>${score.owner}</td>
+          <td>${score.stock}</td>
+          <td>
+            <button class="btn btn-info btn-sm" id="new-score-btn"><i class="fas fa-pen-fancy"></i></button>
+            <button class="btn btn-danger btn-sm" id="new-score-btn"><i class="fas fa-trash-alt"></i></button>
+          </td>
+        </tr>`;
+    }
+    tableBodyNode.innerHTML = injectedString;
+  }
 }
 
 const nic = new App({ name: "nic", password: "1234" });
-nic.createComposer({
+nic.renderStyleSelect();
+nic.renderInstrumentsSelect();
+const composer1 = new Composer({
   name: "composer1",
   isFemale: true,
   period: "XIX",
   nationality: "Spanish",
   funFact: "is silly",
 });
-console.log(nic);
+const composer2 = new Composer({
+  name: "composer2",
+  isFemale: true,
+  period: "XIX",
+  nationality: "Spsfhds",
+  funFact: "is silly",
+});
+nic.addComposer(composer1);
+nic.addComposer(composer2);
 
-nic.createScore({
+const score1 = new Score({
   style: nic.styles[0],
   composer: nic.composers[0],
   instrumentation: [nic.instruments[0], nic.instruments[1]],
@@ -92,3 +160,14 @@ nic.createScore({
   stock: 1,
   owner: "tailleferre",
 });
+const score2 = new Score({
+  style: nic.styles[2],
+  composer: nic.composers[0],
+  instrumentation: [nic.instruments[2], nic.instruments[1]],
+  title: "Jazzy drums",
+  stock: 5,
+  owner: "gusi",
+});
+nic.addScore(score1);
+nic.addScore(score2);
+console.log(nic);
